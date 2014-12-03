53b4a693d2fdef679a844b83716eceaa2a31889e
Richard Kraaijenhagen <stdin+git@riichard.com>
2014-12-03 14:56:26 +0100

    Added more git aliasses

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,23 @@
+# When gisting a file. make it private by default and copy to clipboard
 alias gist "gist -c --private"
+
+# Easy accessible vim
 alias vi vim
+
+# Commandline shortcuts
 alias l ls
+
+# Git aliasses
+
 alias c "git commit"
 alias s "git status"
 alias gl "git log"
+alias gd "git diff"
+alias ga "git add"
+#alias gc "git commit"
+#alias gca "git add -A && git commit"
 
-
+# Necessary stuff for Go
 set -x GOPATH $HOME/go
 
 
