775a2814541d6211c44cce7b0205ced20188f1ad
Richard Kraaijenhagen <stdin+git@riichard.com>
2014-12-10 14:20:43 +0100

    added alias for jirash

--- fish/config.fish
+++ fish/config.fish
@@ -9,13 +9,15 @@ alias l ls
 alias c clear
 
 # Git aliasses
-
 alias gc "git commit"
 alias gs "git status"
 alias gl "git log"
 alias gd "git diff"
 alias ga "git add"
 #alias gca "git add -A && git commit"
+
+# JIRASH
+alias jirash "~/dev/jirash/bin/jirash"
 
 # Necessary stuff for Go
 set -x GOPATH $HOME/go
