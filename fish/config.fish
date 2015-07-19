cbbca3993a4ae07e13b2f185c60133a39f5a9a01
Richard Kraaijenhagen <stdin+git@riichard.com>
2015-07-19 22:02:53 +0700

    updated vimrc;

---
+++ fish/config.fish
@@ -0,0 +1,26 @@
+# When gisting a file. make it private by default and copy to clipboard
+alias gist "gist -c --private"
+
+# Easy accessible vim
+alias vi vim
+
+# Commandline shortcuts
+alias l ls
+alias c clear
+
+# Git aliasses
+alias gc "git commit"
+alias gs "git status"
+alias gl "git log"
+alias gd "git diff"
+alias ga "git add"
+alias gp "git push"
+#alias gca "git add -A && git commit"
+
+# JIRASH
+alias jirash "~/dev/jirash/bin/jirash"
+
+# Necessary stuff for Go
+set -x GOPATH $HOME/go
+
+
