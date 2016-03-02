0c278c7be17f8a177ec6b998f38b8964dc9a2481
Richard Kraaijenhagen <richard.kraaijenhagen@ing.nl>
2016-03-02 10:14:37 +0100

    Better source file loading and alias to i3config/rei3

--- fish/config.fish
+++ fish/config.fish
@@ -44,15 +44,16 @@ alias utilconfig "vim ~/dev/dotfiles/fish/utils.fish"
 alias promptconfig "vim ~/dev/dotfiles/fish/prompt.fish"
 alias vimconfig "vim ~/dev/dotfiles/vim/vimrc"
 alias tmuxconfig "vim ~/dev/dotfiles/tmux/tmux.conf"
+alias i3config "vim ~/dev/dotfiles/i3/config"
 
 # Load the prompt configuration
-source ~/dev/dotfiles/fish/prompt.fish
+. ~/dev/dotfiles/fish/prompt.fish
 
 # Load z.fish - A script that helps to autofill to the latest use directory
-source ~/dev/dotfiles/fish/z.fish
+. ~/dev/dotfiles/fish/z.fish
 
 # Load util scripts 
-source ~/dev/dotfiles/fish/utils.fish
+. ~/dev/dotfiles/fish/utils.fish
 
 # Necessary stuff for Go
 set -x GOPATH $HOME/go
@@ -62,7 +63,7 @@ set -x GOPATH $HOME/go
 #[ -s "$NVM_DIR/nvm.sh" ] and source "$NVM_DIR/nvm.sh" # This loads NVM 
 
 set -x NVM_DIR "/home/yq63el/.nvm"
-source ~/.config/fish/nvm-wrapper/nvm.fish
+. ~/.config/fish/nvm-wrapper/nvm.fish
 
 function setCapsToCTRL
     echo 'Setting Caps Lock key to Control, Linux only!'
@@ -72,7 +73,7 @@ function setCapsToCTRL
 end
 
 # Load the login settings for the proxy from a different file
-source ~/.config/p.fish
+. ~/.config/p.fish
 
 # Configure ING Proxy
 function proxy
@@ -84,36 +85,37 @@ function proxy
     set password $p 
     set proxypath "http://$username:$password@$serverip:$serverport"
 
-    set -x http_proxy $proxypath
-    set -x HTTP_PROXY $proxypath
-    set -x https_proxy $proxypath
-    set -x HTTPS_PROXY $proxypath
-    set -x ftp_proxy $proxypath
-    set -x FTP_PROXY $proxypath
+    set -g -x http_proxy $proxypath
+    set -g -x HTTP_PROXY $proxypath
+    set -g -x https_proxy $proxypath
+    set -g -x HTTPS_PROXY $proxypath
+    set -g -x ftp_proxy $proxypath
+    set -g -x FTP_PROXY $proxypath
 
-    set -x all_proxy $proxypath
-    set -x ALL_PROXY $proxypath
+    set -g -x all_proxy $proxypath
+    set -g -x ALL_PROXY $proxypath
 
-    set -x no_proxy "127.0.0.1, localhost,*.europe.intranet,*.ict.intranet"
-    set -x NO_PROXY "127.0.0.1, localhost,*.europe.intranet,*.ict.intranet"
+    set -g -x no_proxy "127.0.0.1, localhost,*.europe.intranet,*.ict.intranet"
+    set -g -x NO_PROXY "127.0.0.1, localhost,*.europe.intranet,*.ict.intranet"
 end
 
 # Disable Proxy settings
 function noproxy
     echo 'Deleting proxy setting'
-    set -x http_proxy ''
-    set -x HTTP_PROXY ''
-    set -x https_proxy ''
-    set -x HTTPS_PROXY ''
-    set -x ftp_proxy ''
-    set -x FTP_PROXY ''
+    set -ge http_proxy 
+    set -ge HTTP_PROXY
+    set -ge https_proxy 
+    set -ge HTTPS_PROXY
+    set -ge ftp_proxy 
+    set -ge FTP_PROXY
 end
 
 # Make vim the default $EDITOR
 set -U EDITOR 'vim'
 
 # Alias to quickly reload fish 
-alias refish "source ~/dev/dotfiles/fish/config.fish"
+alias refish ". ~/dev/dotfiles/fish/config.fish"
+alias rei3 "i3-msg reload"
 
 # Custom welcome message
 function fish_greeting
